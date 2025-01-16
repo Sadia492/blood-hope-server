bde36304355f007995f75b4733cd120aec416969
Sadia492 <sadiaafrin2792>
2025-01-16 22:28:06 +0600

    districts and upazilas data fetched

--- index.js
+++ index.js
@@ -1,13 +1,60 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+
 const port = process.env.PORT || 9000;
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Pass}@cluster0.dr5qw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const districtsCollection = client
+      .db("bloodHopeDb")
+      .collection("districts");
+    const upazilasCollection = client.db("bloodHopeDb").collection("upazilas");
+    // Connect the client to the server
+    await client.connect();
+
+    app.get("/districts", async (req, res) => {
+      const result = await districtsCollection.find().toArray();
+      res.send(result);
+    });
+    app.get("/upazilas", async (req, res) => {
+      const result = await upazilasCollection.find().toArray();
+      res.send(result);
+    });
+    // Ping the deployment to confirm the connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } catch (error) {
+    console.error("Failed to connect to MongoDB:", error.message);
+  } finally {
+    // Optional: Close the connection if you don't need it persistently
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.get("/", async (req, res) => {
-  res.send("my bloodHope server is running");
+  res.send("my BloodHope server is running");
 });
 
 app.listen(port, () => {
-  console.log("my port is running");
+  console.log(`Server is running on port ${port}`);
 });
